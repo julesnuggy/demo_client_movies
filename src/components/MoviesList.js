a503f4747ff42c647d58734c9f81fb6a7708b574
Julian Ng <julesnuggy@gmail.com>
2023-07-02 18:47:20 +0100

    Show list of all movies on homepage

---
+++ src/components/MoviesList.js
@@ -0,0 +1,68 @@
+import { Link, useSearchParams } from "react-router-dom"
+import Spinner from './Spinner'
+import client from '../utils/client.js'
+
+function MoviesList({ movies, setMovies, isLoading }) {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const handleChange = async event => {
+    const {value, checked} = event.target
+    const directors = searchParams.getAll('director')
+    if (checked) directors.push(value)
+    if (!checked) directors.splice(directors.indexOf(value), 1)
+    setSearchParams({director: directors})
+    const filteredMovies = await filterByDirectors(directors)
+    setMovies(filteredMovies)
+  }
+
+  const filterByDirectors = async (directors) => {
+    const { movies } = await client.get('/movies')
+    if (directors.length === 0) return movies
+    return movies.filter(movie => directors.includes(movie.director))
+  }
+
+  const handleDelete = async id => {
+    await client.delete(`/movies/${id}`, { method: 'DELETE' })
+    const filteredMovies = movies?.filter(movie => movie.id !== id)
+    setMovies(filteredMovies)
+  }
+
+  return (
+    <>
+    <header>
+      <h2>Movies</h2>
+    </header>
+
+    { isLoading ?
+      <Spinner /> :
+      <>
+        <label className="filter">
+          <input name="director" type="checkbox" value="personal"  onChange={handleChange} />
+          <span>🍻</span> Personal
+        </label>
+        <label className="filter">
+          <input name="director" type="checkbox" value="work"  onChange={handleChange} />
+          <span>💻</span> Work
+        </label>
+        <ul className="contacts-list">
+          {movies.map(movie => {
+            return (
+              <li className="contact" key={movie.id}>
+                <b>{movie.title}</b>
+                <p>Director: {movie.director}</p>
+                <p>Release Year: {movie.release_year}</p>
+                <p>
+                  <Link to={`/movies/${movie.id}`}>View</Link>
+                  <Link to={`/movies/${movie.id}/edit`} state={{ movie }}>Edit</Link>
+                  <a href="#" onClick={() => handleDelete(movie.id)}>Delete</a>
+                </p>
+              </li>
+            )
+          })}
+        </ul>
+      </>
+    }
+    </>
+  )
+}
+
+export default MoviesList;
